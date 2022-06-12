79967f967f0b1a1f0d354e754a7f2df73c9104d6
Bushra Shireen <sbushra109@gmail.com>
2022-06-12 13:56:36 +0530

    Quest3

---
+++ Day3/reverseStack.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name="Bushra";
+    stack<char> s;
+
+for(int i=0;i<name.length();i++){
+    char ch=name[i];
+    s.push(ch);
+
+}
+string ans="";
+while(!s.empty()){
+    char ch=s.top();
+    ans.push_back(ch);
+    s.pop();
+}
+cout<<endl<<ans;
+return 0;
+}
