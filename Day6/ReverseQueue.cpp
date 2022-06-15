9aae5065d84d6225b8ac1346cdcbf3889a90e25d
Bushra Shireen <sbushra109@gmail.com>
2022-06-15 11:43:35 +0530

    Quest1

---
+++ Day6/ReverseQueue.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+queue<int> rev(queue<int> q)
+{
+    stack<int> s;
+
+    while(!q.empty()){
+      int num=q.front();
+      s.push(num);
+      q.pop();
+    }
+
+    while(!s.empty()){
+        int num=s.top();
+        q.push(num);
+        s.pop();
+    }
+   return q;
+}
+    
